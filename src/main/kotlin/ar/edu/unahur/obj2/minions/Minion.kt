d7110bc4b7899e8792388883d3786a434961076b
Nicolas D <profesionalnicolasdiaz@gmail.com>
2020-10-17 01:51:44 -0300

    Minion - clase y raza - incompleto

--- src/main/kotlin/ar/edu/unahur/obj2/minions/Minion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/minions/Minion.kt
@@ -1,2 +1,48 @@
 package ar.edu.unahur.obj2.minions
 
+import jdk.internal.joptsimple.internal.Strings
+
+class Minion(var clase: Clase, val raza: Raza,var estamina: Float,var danio: Float) {
+    fun estaContento() = estamina >= 30
+    //fun defender(estamina: Float) =
+}
+
+abstract class Clase {
+    //open fun defender() = (estamina/2) + 2
+}
+
+class Soldado : Clase() {
+    val arma = String()
+    //var danioMejorando = danio
+    //override fun defender() {
+
+    //    this.ganarPractica()
+    //}
+
+    //fun ganarPractica() = danioMejorando + 2
+}
+
+object Obrero : Clase() {
+    var cinturon =  mutableListOf<Strings>()
+
+
+}
+
+object Limpiador : Clase() {
+    //override fun defender(): Nothing = throw Exception("Me niego")
+
+}
+
+interface Raza
+
+object Biclopes: Raza {
+    val ojos = 2
+    val punteria = 1
+    fun estamina(estamina: Int) = estamina.plus(10)
+}
+
+object Ciclopes: Raza {
+    val ojos = 1
+    val punteria = 0.5
+    var estamina = Float
+}
