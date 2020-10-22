f579c3b2cb9b7a5acb5cee4290e857406d81f771
Nicolas D <profesionalnicolasdiaz@gmail.com>
2020-10-22 02:54:35 -0300

    unaTarea.realizarsePor(this.?seleccionarElMejor())

--- src/main/kotlin/ar/edu/unahur/obj2/minions/Laboratorio.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/minions/Laboratorio.kt
@@ -8,7 +8,7 @@ class Laboratorio{
     var empleados = mutableListOf<Minion>()
 
     fun enviarTarea(minion: Minion, tarea: Tarea) {
-        if (tarea.puedeSerRealizada(minion))   minion.tareaRealizadas.add(tarea)
+        if (tarea.puedeSerRealizada(minion)) minion.tareaRealizadas.add(tarea)
         else throw Exception("no puede realizarla la tarea no cumple con los requisitos....")
     }
     fun asignarRol(minion: Minion,rolNuevo: Rol){ minion.rol = rolNuevo}
--- src/main/kotlin/ar/edu/unahur/obj2/minions/Rol.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/minions/Rol.kt
@@ -37,3 +37,26 @@ object Obrero : Rol() {
 object Limpiador : Rol() {
     override fun defender(minion: Minion) = throw Exception("Me niego")
 }
+
+/* hay que repensar quien conviene que recuerde o ejecute las cosas si hacer
+    unaTarea.puedeSerRealizada(unMinion) o unMinion.puedeRealizar(unaTarea)
+    en todas las tareas es necesario el dato de un minion como parametro
+    en capataz se complica.
+*/
+// mismas caracteristicas que obrero
+object Capataz: Rol(){
+    val subAlternos = mutableListOf<Minion>()
+    fun realizarTarea(unaTarea: Tarea){
+        // unaTarea.realizarsePor(this.?seleccionarElMejor())
+
+
+    }
+
+    fun seleccionarElMejor() = subAlternos.maxBy{ it.experiencia() }
+    fun empladosPuedenHacerla(tarea: Tarea) {
+        subAlternos.any {  tarea.puedeSerRealizada(it)}
+    }
+
+
+
+}
